42e184a87c661ae12499966da871f2b9ad87be99
NagendraVarma1 <nagendravarmal512@gmail.com>
2023-12-05 10:30:18 +0530

    task-7, vid-8

--- models/product.js
+++ models/product.js
@@ -3,40 +3,36 @@ const path = require("path");
 
 const products = [];
 
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+const getProductsFormFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-        let products = [];
-        if(!err) {
-            products = JSON.parse(fileContent)
-        }
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), (err) => {
-            console.log(err)
-        })
+    getProductsFormFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
     });
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-        path.dirname(process.mainModule.filename),
-        "data",
-        "products.json"
-      );
-    fs.readFile(p, (err, fileContent) => {
-        if(err) {
-           cb([]);
-        }
-        cb(JSON.parse(fileContent))
-    })
+    getProductsFormFile(cb);
   }
 };
